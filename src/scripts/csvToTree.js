dbe319673804a632ba0d65c96c53066e3492dbe9
PowVT <epowlowsky@gmail.com>
2022-08-03 08:51:02 -0400

    nodes created

--- src/scripts/csvToTree.js
+++ src/scripts/csvToTree.js
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import * as d3 from "d3";
+import { hierarchy, tree, scaleLinear } from 'd3';
 
-const DEBUG = true;
+const DEBUG = false;
 
 // *** STATE ***
 let root;
@@ -37,3 +38,39 @@ parsedData.forEach(function (node) {
 if (DEBUG) console.log(treeData);
 
 root = treeData[0];
+
+const h = hierarchy(root);
+
+h.descendants().forEach((d, i) => {
+    d.id = `${i}`;
+    d._children = d.children;
+    d.children = null;
+});
+
+if (DEBUG) console.log(h._children)
+
+const layout = tree().nodeSize([25, 200]);
+
+const colorScale = scaleLinear().domain([0, 5]).range(['#ff0072', '#0041d0']);
+
+function getElements() {
+    const root = layout(h);
+
+    const nodes = root.descendants().map((d) => ({
+        id: d.id,
+        data: { label: d.data.name, depth: d.depth },
+        position: { x: d.y, y: d.x },
+        style: { padding: 0, backgroundColor: colorScale(d.depth), border: 'none', color: 'white', fontWeight: 'bold' },
+        //sourcePosition: Position.Right,
+        //targetPosition: Position.Left,
+        type: d._children ? 'default' : 'output',
+    }));
+
+    const edges = root.links().map((d, i) => ({ id: `${i}`, source: d.source.id, target: d.target.id }));
+
+    return { nodes, edges };
+}
+
+const initialElements = getElements();
+
+console.log(initialElements.nodes);
